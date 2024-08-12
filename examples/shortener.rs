cc36d775f405db9183d7e17084987def7f2afe04
jonjia <jiawenhuiwar3@163.com>
2024-08-12 23:19:33 +0800

    feat: use result

--- examples/shortener.rs
+++ examples/shortener.rs
@@ -9,7 +9,8 @@ use axum::{
 use serde::{Deserialize, Serialize};
 use sqlx::{FromRow, PgPool};
 use tokio::net::TcpListener;
-use tracing::info;
+use tracing::{info, level_filters::LevelFilter};
+use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
 
 #[derive(Debug, Deserialize)]
 struct ShortenRequest {
@@ -37,30 +38,40 @@ struct UrlRecord {
 const BASE_URL: &str = "0.0.0.0:3000";
 
 #[tokio::main]
-async fn main() {
-    let pool = PgPool::connect("postgres://postgres:password@localhost/shortener")
-        .await
-        .unwrap();
-    let state = AppState { db: pool };
-    let app = Router::new()
+async fn main() -> Result<()> {
+    let layer = Layer::new().pretty().with_filter(LevelFilter::INFO);
+    tracing_subscriber::registry().with(layer).init();
+
+    let db_url = "postgres://postgres:password@localhost/shortener";
+    let state = AppState::try_new(db_url).await?;
+    info!("Connected to database, {}", db_url);
+
+    let listener = TcpListener::bind(BASE_URL).await?;
+    info!("Listening on {}", BASE_URL);
+
+    let router = Router::new()
         .route("/", post(shorten))
         .route("/:id", get(redirect))
         .with_state(state);
-    let server = TcpListener::bind(BASE_URL).await.unwrap();
 
-    info!("Listening on {}", BASE_URL);
+    serve(listener, router).await?;
 
-    serve(server, app).await.unwrap();
+    Ok(())
 }
 
 async fn shorten(
     State(state): State<AppState>,
-    Json(request): Json<ShortenRequest>,
-) -> impl IntoResponse {
-    let id = state.shorten(request.url).await.unwrap();
-    Json(ShortenResponse {
+    Json(data): Json<ShortenRequest>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let id = state.shorten(&data.url).await.map_err(|e| {
+        info!("Failed to shorten URL: {}", e);
+        StatusCode::UNPROCESSABLE_ENTITY
+    })?;
+    let body = Json(ShortenResponse {
         url: format!("http://{}/{}", BASE_URL, id),
-    })
+    });
+
+    Ok((StatusCode::CREATED, body))
 }
 
 async fn redirect(
@@ -73,11 +84,27 @@ async fn redirect(
         .map_err(|_| StatusCode::NOT_FOUND)?;
     let mut headers = HeaderMap::new();
     headers.insert(LOCATION, url.parse().unwrap());
-    Ok((headers, StatusCode::FOUND))
+    Ok((StatusCode::PERMANENT_REDIRECT, headers))
 }
 
 impl AppState {
-    async fn shorten(&self, url: String) -> Result<String> {
+    async fn try_new(url: &str) -> Result<Self> {
+        let pool = PgPool::connect(url).await?;
+        // create table if not exists
+        sqlx::query(
+            r#"
+            CREATE TABLE IF NOT EXISTS urls (
+                id CHAR(6) PRIMARY KEY,
+                url TEXT NOT NULL UNIQUE
+            );"#,
+        )
+        .execute(&pool)
+        .await?;
+
+        Ok(Self { db: pool })
+    }
+
+    async fn shorten(&self, url: &str) -> Result<String> {
         let id = nanoid::nanoid!(6);
         let ret: UrlRecord = sqlx::query_as(
             "INSERT INTO urls(id, url) VALUES ($1, $2) ON CONFLICT(url) DO UPDATE SET url=EXCLUDED.url RETURNING id;",
