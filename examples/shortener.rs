eda95b95fc0f0385fda9bb07cb3574703d3af48d
勿待 <jiawenhui.jwh@alibaba-inc.com>
2024-08-14 09:24:25 +0800

    feat: add custom error

--- examples/shortener.rs
+++ examples/shortener.rs
@@ -1,14 +1,19 @@
+use std::time::Duration;
+
 use anyhow::Result;
 use axum::{
-    extract::{Path, State},
+    error_handling::HandleErrorLayer,
+    extract::{rejection::JsonRejection, Path, State},
     http::{header::LOCATION, HeaderMap, StatusCode},
-    response::IntoResponse,
+    response::{IntoResponse, Response},
     routing::{get, post},
-    serve, Json, Router,
+    serve, BoxError, Json, Router,
 };
 use serde::{Deserialize, Serialize};
 use sqlx::{FromRow, PgPool};
+use thiserror::Error;
 use tokio::net::TcpListener;
+use tower::{timeout::error::Elapsed, ServiceBuilder};
 use tracing::{info, level_filters::LevelFilter};
 use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
 
@@ -35,7 +40,25 @@ struct UrlRecord {
     url: String,
 }
 
-const BASE_URL: &str = "0.0.0.0:3000";
+#[derive(Debug, Error)]
+enum AppError {
+    #[error("I/O error: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("Failed to parse JSON: {0}")]
+    JsonRejection(#[from] JsonRejection),
+
+    #[error("Database error: {0}")]
+    Db(#[from] sqlx::Error),
+
+    #[error("Timeout error: {0}, request took too long, max time is 1ms")]
+    Timeout(#[from] Elapsed),
+
+    #[error("Internal server error {0}")]
+    InternalServer(#[from] anyhow::Error),
+}
+
+const BASE_URL: &str = "0.0.0.0:9876";
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -46,12 +69,17 @@ async fn main() -> Result<()> {
     let state = AppState::try_new(db_url).await?;
     info!("Connected to database, {}", db_url);
 
-    let listener = TcpListener::bind(BASE_URL).await?;
+    let listener = TcpListener::bind(BASE_URL).await.map_err(AppError::Io)?;
     info!("Listening on {}", BASE_URL);
 
     let router = Router::new()
         .route("/", post(shorten))
         .route("/:id", get(redirect))
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(handle_timeout_error))
+                .timeout(Duration::from_secs(30)),
+        )
         .with_state(state);
 
     serve(listener, router).await?;
@@ -62,11 +90,11 @@ async fn main() -> Result<()> {
 async fn shorten(
     State(state): State<AppState>,
     Json(data): Json<ShortenRequest>,
-) -> Result<impl IntoResponse, StatusCode> {
-    let id = state.shorten(&data.url).await.map_err(|e| {
-        info!("Failed to shorten URL: {}", e);
-        StatusCode::UNPROCESSABLE_ENTITY
-    })?;
+) -> Result<impl IntoResponse, AppError> {
+    let id = state
+        .shorten(&data.url)
+        .await
+        .map_err(AppError::InternalServer)?;
     let body = Json(ShortenResponse {
         url: format!("http://{}/{}", BASE_URL, id),
     });
@@ -77,18 +105,25 @@ async fn shorten(
 async fn redirect(
     State(state): State<AppState>,
     Path(id): Path<String>,
-) -> Result<impl IntoResponse, StatusCode> {
-    let url = state
-        .get_url(&id)
-        .await
-        .map_err(|_| StatusCode::NOT_FOUND)?;
+) -> Result<impl IntoResponse, AppError> {
+    let url = state.get_url(&id).await.map_err(AppError::InternalServer)?;
     let mut headers = HeaderMap::new();
     headers.insert(LOCATION, url.parse().unwrap());
     Ok((StatusCode::PERMANENT_REDIRECT, headers))
 }
 
+async fn handle_timeout_error(err: BoxError) -> Result<(), AppError> {
+    if err.is::<Elapsed>() {
+        Err(AppError::Timeout(Elapsed::new()))
+    } else {
+        Err(AppError::InternalServer(anyhow::Error::msg(
+            "Internal server error",
+        )))
+    }
+}
+
 impl AppState {
-    async fn try_new(url: &str) -> Result<Self> {
+    async fn try_new(url: &str) -> Result<Self, AppError> {
         let pool = PgPool::connect(url).await?;
         // create table if not exists
         sqlx::query(
@@ -122,5 +157,21 @@ impl AppState {
             .fetch_one(&self.db)
             .await?;
         Ok(record.url)
+    }
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        let (status, message) = match self {
+            AppError::JsonRejection(rejection) => (rejection.status(), rejection.body_text()),
+            AppError::Timeout(err) => (StatusCode::REQUEST_TIMEOUT, err.to_string()),
+            AppError::InternalServer(err) => (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()),
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Internal server error".to_owned(),
+            ),
+        };
+
+        (status, message).into_response()
     }
 }
